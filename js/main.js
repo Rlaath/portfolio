3fd056d832bb9e248e619a6c5af5efdef7ff3747
Sohee-laptop <sohee7170@naver.com>
2023-02-18 10:20:20 +0900

    js.navbar menu click

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 'use strict'
 
+
+// navbar move when scrolled
 const navbar = document.querySelector('.navbar');
 const navbarheight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
-  console.log(window.scrollY);
-  console.log(`navbarheight = ${navbar.getBoundingClientRect().height}`)
-
   if(window.scrollY > navbarheight){
     navbar.classList.add('navbar__dark')
   }else{
     navbar.classList.remove('navbar__dark')
   }
 });
+
+//go to the id
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event)=>{
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if(link == null){
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth",block : "center" ,inline : "center"});
+
+
+})
+
